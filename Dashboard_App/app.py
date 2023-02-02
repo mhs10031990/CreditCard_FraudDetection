1efef2c21002bfc1e1281075a08418e67ccbee93
ms10031990 <ms10031990>
2023-02-02 03:29:33 +0000

    auto save by knights-watch

--- Dashboard_App/app.py
+++ Dashboard_App/app.py
@@ -71,11 +71,20 @@ with tab2:
 with tab3:
     if check_prediction > 0:
         st.text_input("Model Input", payload)
-        #score_input = {"payload" : payload}
-        #data_1 = pd.DataFrame.from_dict(payload)
-        #st.table(score_input)
+        headers={"Content-type":"application/json"}
+        url = 'http://svc-090535dc-7b28-45c0-ae26-cf4b6523c34f:5001/creditcardfrauddetectionmodel/8adb1856-5c6a-45bd-93bf-1e0c568e8e59/score'
+        score_input = {"payload" : payload}
+        st.write(score_input)
+        response_json = requests.post(url, json=data, headers=headers)
+        st.text_input("API Response: ",response_json.content)
+        response = response_json.json()
+        try:
+            output1 = response['upload_logging_data']['response_data']
+            st.text_input ('Binary Model Output: ',output1)
+        except ValueError:
+            output1 =  response.status_code 
+            st.text_input ('Oops we got an error: ',output1)
         output1 = binary_model(payload)
-        st.text_input ('Binary Model Output',output1)
         st.write("#")
     else:
         st.write("Submit to check the model prediction")
